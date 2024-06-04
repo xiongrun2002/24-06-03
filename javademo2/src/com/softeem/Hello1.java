002ab9fae425ca74134fcbdaf00889f9fc1d1fa5
xr <2927743854@qq.com>
2024-06-04 14:39:23 +0800

    再次提交 Hello1 类

--- javademo2/src/com/softeem/Hello1.java
+++ javademo2/src/com/softeem/Hello1.java
@@ -1,7 +1,10 @@
 package com.softeem;
 
 public class Hello1 {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("完蛋");
     }
 }
